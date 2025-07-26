1dd479f5db37e93ea6c623596504224972489062
Nihesh Rachakonda <niheshr03@gmail.com>
2025-07-27 00:23:42 +0530

    s

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import path from "path";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/inmin-try-find-magic/",
+  base: "./inmin-try-find-magic/",
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
