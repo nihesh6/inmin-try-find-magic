45218f3e11fa83006bea580cb1a88167440efac3
Nihesh Rachakonda <niheshr03@gmail.com>
2025-07-27 00:22:02 +0530

    qwq

--- vite.config.ts
+++ vite.config.ts
@@ -1,21 +1,14 @@
 import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react-swc";
+import react from "@vitejs/plugin-react";
 import path from "path";
-import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
-export default defineConfig(({ mode }) => ({
-  server: {
-    host: "::",
-    port: 8080,
-  },
-  plugins: [react(), mode === "development" && componentTagger()].filter(
-    Boolean
-  ),
+export default defineConfig({
+  plugins: [react()],
+  base: "/inmin-try-find-magic/",
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
     },
   },
-  base: "/inmin-try-find-magic/",
-}));
+});
